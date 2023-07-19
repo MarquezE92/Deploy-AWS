82f1465aa6b735ca7ac2a0ea8940cd54a9899684
MarquezE92 <vsuykyasmbp@hotmail.com>
2023-07-18 23:51:16 -0300

    add boiler front

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Deployment en AWS
+      </header>
+    </div>
+  );
+}
+
+export default App;
