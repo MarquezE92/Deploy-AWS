c62d2db797499d18f188f4f44ee672b51b6e2ed6
MarquezE92 <vsuykyasmbp@hotmail.com>
2023-07-19 00:33:23 -0300

    finish front for workshop

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
-
+import {useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+const [info, setInfo] = useState("")
+
+function callInfo() {
+  axios.get('http://localhost:3002/info')
+    .then((response) => {
+      setInfo(response.data[0]["content"])
+    })
+    .catch(error => {
+      console.error('Error loading info:', error);
+    
+    })
+}
+
   return (
     <div className="App">
       <header className="App-header">
-        Deployment en AWS
+        <h1>Deployment en AWS</h1>
+        <button onClick={callInfo}>Llamar a la información de la base de datos</button>
+        <p>{info}</p>
       </header>
     </div>
   );
